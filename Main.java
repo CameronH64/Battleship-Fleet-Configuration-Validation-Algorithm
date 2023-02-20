6dcd5003362860f8e1f72be6595c3cbe9b623cec
CameronH64 <cholbrook582@gmail.com>
2023-02-19 22:58:53 -0600

    Minor polish.

--- Main.java
+++ Main.java
@@ -40,7 +40,7 @@ public class Main {
 		 * When a letter is hit, check the next element to see if it matches.
 		 * If it matches, it's horizontal. If not, it's vertical.
 		 * 
-		 * If horiztonal, check that there are that many sequential letters.
+		 * If horizontal, check that there are that many sequential letters.
 		 * If vertical, check if there are that many sequential letters with 10 space in between them.
 		 * 
 		 * This actually inherently takes care of placing boats in an L-shape.
@@ -53,8 +53,6 @@ public class Main {
 		
 		
 		ArrayList<String> flatFleet = new ArrayList<String>();
-		String shipID[] = {"C", "B", "D", "S", "P"};
-		int shipSize[] = {5, 4, 3, 3, 2};
 		int totalShipSize = 0;
 		
 		int runningCarrierCount = 0;
@@ -63,16 +61,18 @@ public class Main {
 		int runningSubmarineCount = 0;
 		int runningPatrolCount = 0;
 		
-		boolean returnType = false;
+		
 		
 		// First, flatten the array into a 1D array.
-		for(int i = 0; i < 10; i++) {		// Check each row.
-			for(int j = 0; j < 10; j++) {	// Check each column.
+		for(int i = 0; i < 10; i++) {				// Check each row.
+			for(int j = 0; j < 10; j++) {			// Check each column.
 				flatFleet.add(fleetArray[i][j]);
 			}
 		}
 		
-		// Debugging
+		
+		
+		// Debugging; print out the whole 1D array.
 		for (String string : flatFleet) {
 			System.out.print(string);
 		}
@@ -91,21 +91,23 @@ public class Main {
 		
 		
 		// Do ship checking.
+		// The comments for the carrier are the same for the other four ships.
 		
 		// Carrier
-		for(int i = 0; i < 100; i++) {
-			if(flatFleet.get(i) == "C") {
-				if(flatFleet.get(i+1) == "C") {	// Check orientation of ship.
+		for(int i = 0; i < 100; i++) {				// Cycle through 1D array.
+			if(flatFleet.get(i) == "C") {			// ... Until it hits a carrier ship (represented by a C).
+				if(flatFleet.get(i+1) == "C") {		// Check if orientation is vertical.
 				
-					for(int j = 0; j < 5; j++) {
+					for(int j = 0; j < 5; j++) {					// If vertical, check the C's in the downward direction.
 						if(flatFleet.get(i+j).equals("C")) {
 							runningCarrierCount++;
 						}
 					}
 					break;
 				} else {
-					for(int j = 0; j < 5; j++) {
-						if(flatFleet.get(i+(j*10)).equals("C")) {
+					
+					for(int j = 0; j < 5; j++) {	// Check if orientation is horizontal.	
+						if(flatFleet.get(i+(j*10)).equals("C")) {		// If vertical, check the C's in the downward direction.
 							runningCarrierCount++;
 						}
 					}
@@ -128,6 +130,7 @@ public class Main {
 					}
 					break;
 				} else {
+					
 					for(int j = 0; j < 4; j++) {
 						if(flatFleet.get(i+(j*10)).equals("B")) {
 							runningBattleshipCount++;
@@ -151,6 +154,7 @@ public class Main {
 					}
 					break;
 				} else {
+					
 					for(int j = 0; j < 3; j++) {
 						if(flatFleet.get(i+(j*10)).equals("D")) {
 							runningDestroyerCount++;
@@ -173,6 +177,7 @@ public class Main {
 					}
 					break;
 				} else {
+					
 					for(int j = 0; j < 3; j++) {
 						if(flatFleet.get(i+(j*10)).equals("S")) {
 							runningSubmarineCount++;
@@ -196,6 +201,7 @@ public class Main {
 					}
 					break;
 				} else {
+					
 					for(int j = 0; j < 3; j++) {
 						if(flatFleet.get(i+(j*10)).equals("P")) {
 							runningPatrolCount++;
@@ -207,7 +213,10 @@ public class Main {
 		}
 
 		
-		
+		// If every single battleship is the length that they should be, then it's a valid fleet configuration.
+		// This is why the check for 17 spaces is not sufficient.
+		// For example, a carrier could be 6 spaces and a battleship could be 3.
+		// That method alone would fail this test.
 		if(runningCarrierCount == 5 && runningBattleshipCount == 4 && runningDestroyerCount == 3 && runningSubmarineCount == 3 && runningPatrolCount == 2) {
 			return true;
 		}
